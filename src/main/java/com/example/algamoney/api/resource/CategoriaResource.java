1f56fb39d5f711b7720b7cce8d1decacb2057399
ealvess <everton10alves@gmail.com>
2022-07-15 18:02:54 -0300

    Cadastrando nova categoria com POST

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,19 +1,27 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 
-@RestController //controlador rest, o retornor já é facilitado, converte para json
+@RestController //controlador rest, o retorno já é facilitado, converte para json
 @RequestMapping("/categorias") //faz o mapeamento da requisição
 public class CategoriaResource {
 	
+	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
@@ -23,5 +31,17 @@ public class CategoriaResource {
 		//se a lista não estiver vazia, responde com 200 ok passando as categorias, caso esteja vazia, retorna
 		//nocontent status code 204, ou seja, deu certo no servidor mas não tem nada para mostrar. 
 		//o build é colocado para gerar um responseentity
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		//através da classe ServletUriComponentsBuilder eu vou pegar a partir da uri da requisição atual, adicionar o código
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}") 
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();// e adicionar esse codigo na uri
+		response.setHeader("Location", uri.toASCIIString());//setar o header location com esta uri
+		
+		return ResponseEntity.created(uri).body(categoriaSalva); //retorna a categoria criada
 	}
 }
